734c476032456cd3d7a59a6c99458ec5bc8acc89
Snehagupta2005 <snehagupta6298@gmail.com>
2025-05-17 20:22:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Show alert when button is clicked
+document.getElementById('orderButton').addEventListener('click', () => {
+  alert("Thanks for your interest! 🎉 We'll get in touch to take your cake order.");
+});
